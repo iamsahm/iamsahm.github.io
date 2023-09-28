eba0c458c178f37492689fa42e3e42ce474ec299
sam <sam.ryan101@gmail.com>
2023-09-28 20:48:21 +0100

    deploy yaml

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,8 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// Determine the base URL for GitHub Pages based on your repository name
-const base = "/your-repository-name/"; // Replace with your repository name
-
 // https://vitejs.dev/config/
 export default defineConfig({
-    base, // Set the base URL
     plugins: [vue()],
     resolve: {
         alias: {
