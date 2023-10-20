bdedf4e0cbb7770c33eaf4c4ba97872dcaf29312
sam <sam.ryan101@gmail.com>
2023-10-20 10:13:34 +0100

    added history, updated title

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import ExternalRedirectView from "../views/ExternalRedirectView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
+    mode: "history",
     routes: [
         {
             path: "/",
