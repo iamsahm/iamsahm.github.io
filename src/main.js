9f54be3f594d1148cebce7324e628ba0064148fa
sam <sam.ryan101@gmail.com>
2023-10-30 13:38:18 +0000

    google analytics installed

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
-import './assets/main.css'
+import "./assets/main.css";
+import { createApp } from "vue";
+import App from "./App.vue";
+import VueGtag from "vue-gtag";
+import router from "./router";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+const app = createApp(App);
 
-const app = createApp(App)
+app.use(router);
 
-app.use(router)
+app.use(VueGtag, {
+    config: { id: "G-Z47MQ575DY" },
+    appName: "Sam Ryan Portfolio",
+    pageTrackerScreenviewEnabled: true,
+    router,
+});
 
-app.mount('#app')
+app.mount("#app");
